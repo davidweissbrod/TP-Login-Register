d05c15fc08f91c66496b7b8374edbf2b4ba3bfe6
davidweissbrod <47436755@est.ort.edu.ar>
2024-10-16 20:01:21 -0300

    detalls

--- tp-login-register/App.js
+++ tp-login-register/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/Login';
-import HomeScreen from './src/screens/Home';
-import RegisterScreen from './src/screens/Register'; 
+import LoginScreen from './src/screens/Login.js';
+import HomeScreen from './src/screens/Home.js';
+import RegisterScreen from './src/screens/Register.js'; 
+import NewEventScreen from './src/screens/NewEvent.js'
+import EditEventScreen from './src/screens/EditEvent.js'
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Nuevo Evento" component={NewEventScreen} />
+        <Stack.Screen name="Editar Evento" component={EditEventScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
